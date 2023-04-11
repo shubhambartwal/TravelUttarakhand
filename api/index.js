75abec2fe5ba417aa3c6981f45ef52749f86d343
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-04-11 09:22:38 +0530

    added jwt,cookie-parser in auth

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 const connect = async () => {
@@ -19,6 +20,7 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected");
 });
 //middlewares
+app.use(cookieParser())
 app.use(express.json())
 app.use('/api/auth',authRoute);
 app.use('/api/users',usersRoute);
